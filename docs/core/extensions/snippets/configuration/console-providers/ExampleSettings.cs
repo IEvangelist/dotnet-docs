6b20e80a87435e5bcfa401e643424f3a073e502e
David Pine <david.pine.7@gmail.com>
2024-03-07 07:41:07 -0600

    WIP

---
+++ docs/core/extensions/snippets/configuration/console-providers/ExampleSettings.cs
@@ -0,0 +1,11 @@
+﻿public record class ExampleSettings
+{
+    public string? Source { get; set; } = "Declared in code";
+    public int Value { get; set; } = -1;
+    public string? One { get; set; }
+    public string? Two { get; set; }
+    public string? Three { get; set; }
+    public string? Four { get; set; }
+    public string? Five { get; set; }
+    public string? Six { get; set; }
+}
---
+++ docs/core/extensions/snippets/configuration/console-providers/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.Configure<ExampleSettings>(
+    builder.Configuration.GetSection("ExampleSettings"));
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+var settings = app.Services.GetRequiredService<IOptions<ExampleSettings>>().Value;
+
+logger.LogInformation("{Settings}", settings);
+
+var config = (IConfigurationRoot)app.Services.GetRequiredService<IConfiguration>();
+
+IConfigurationProvider[] providers = [.. config!.Providers];
+
+for (var i = 0; i < providers.Length; ++i)
+{
+    var provider = providers[i];
+
+    logger.LogInformation("{Provider}", provider);
+}
+
+await app.RunAsync();
